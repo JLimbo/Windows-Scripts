2ba1c5c9b3a899bd7df74ec2e5f8e82714cf5e50
john <john.limb@iress.com>
2021-03-17 10:41:08 +0000

    added more files and readme

---
+++ Create Local Admin/MakeALocalAdmin.ps1
@@ -0,0 +1,28 @@
+﻿To create local admin account.
+
+# The default password for the new accounts
+$password = ConvertTo-SecureString "" -AsPlainText -Force
+
+# Group new users are added to (default is Users)
+$group = "Administrators"
+
+# PowerShell array containing the users to be created
+$users = @(
+	# List users here to be created
+	""
+)
+
+# Creates a new users for each entry in the $users array
+foreach ($user in $users) {
+	
+	# Create and add user to Users group
+	New-LocalUser -Name "$user" -Password $Password
+	Add-LocalGroupMember -Group "$group" -Member "$user"
+	
+	# Set users password to be changed at next logon
+	$expUser = [ADSI]"WinNT://localhost/$user,user"
+	$expUser.passwordExpired = 0
+	$expUser.setinfo()
+	
+	}
+Set-LocalUser -Name "" -PasswordNeverExpires 1
