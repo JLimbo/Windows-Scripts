4a1d848bfef5ff54cdc5c3810e6e62c861b57f67
john <john.limb@iress.com>
2021-03-17 10:26:41 +0000

    updated day to run

--- WeeklyReboot-Task/WeeklyReboot-Task.bat
+++ WeeklyReboot-Task/WeeklyReboot-Task.bat
@@ -1,4 +1,4 @@
 color a 
 title Creating weekly reboot task
 rem to make machine reboot weekly
-schtasks /create /tn weeklyReboot /tr "shutdown /r /t 0" /sc weekly /st 00:00 /sd 20/03/2021 /ru "System"
+schtasks /create /tn weeklyReboot /tr "shutdown /r /t 0" /sc weekly /st 00:00 /sd 20/03/2021 /d SUN /ru "System"
